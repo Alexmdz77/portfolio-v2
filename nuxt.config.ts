0a673e9a3ceafc68dca1f27e2b81faaf42d545c0
Alexmdz77 <alext77165@gmail.com>
2023-01-20 21:40:11 +0100

    ✨ Add google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: true,
+    modules: [
+        ['@nuxtjs/google-fonts',],
+    ],
+    googleFonts: {
+        families: {
+            'Sora': [400, 500, 600, 700, 800, 900],
+        },
+        download: true,
+        preload: true,
+    },
     vite: {
         css: {
             preprocessorOptions: {
@@ -21,14 +31,6 @@ export default defineNuxtConfig({
         htmlAttrs: {
           lang: 'en'
         },
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico', as: 'image' },
-            { rel: 'preload', href: '~/assets/fonts/Sora-Light.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
-            { rel: 'preload', href: '~/assets/fonts/Sora-Regular.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
-            { rel: 'preload', href: '~/assets/fonts/Sora-Medium.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
-            { rel: 'preload', href: '~/assets/fonts/Sora-SemiBold.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
-            { rel: 'preload', href: '~/assets/fonts/Sora-Bold.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
-        ]
       }
     }
 })
