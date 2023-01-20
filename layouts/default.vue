59ad18ff2fe4f136ae41b26c7e1dd599f66b9637
Alexmdz77 <alext77165@gmail.com>
2023-01-20 21:18:12 +0100

    🚧 Add wip

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+    <div class="wip">Work in progress...</div>
     <Navbar />
     <div id="content">
       <slot/>
@@ -10,6 +11,16 @@
 </script>
 
 <style lang="scss" scoped>
+  .wip {
+    position: fixed;
+    top: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 16px;
+    font-weight: 700;
+    color: #fff;
+    z-index: 1000;
+  }
   #content {
     max-width: 1250px;
     padding: 230px 0;
